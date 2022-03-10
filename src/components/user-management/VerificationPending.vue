c0027486c477604f4b248b513a9f8f5160b1254b
my-legacy.rocks <director@emailn.de>
2022-03-10 15:39:33 +0100

    chore: added warning about verification issue in firefox browser

--- src/components/user-management/VerificationPending.vue
+++ src/components/user-management/VerificationPending.vue
@@ -5,6 +5,11 @@
       Thank you for signing up. To activate your account click on the link in
       the verification email which was sent to {{ email }}.
     </div>
+    <div v-if="firefox">
+      ⚠ There is currently an issue in <b>firefox</b> with account verification.
+      In case you have already verified your email, just logout and login again
+      to proceed to the dashboard. ⚠
+    </div>
     <button
       class="btn btn-dark btn-lg btn-block mt-2"
       type="button"
@@ -35,6 +40,9 @@ export default defineComponent({
   computed: {
     email() {
       return this.$store.state.email;
+    },
+    firefox() {
+      return navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
     },
   },
   methods: {
